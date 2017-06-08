e53347ca6383e74863a50e84e20b3d7ba2c71650
Sashok15 <lordyhard@gmail.com>
2017-06-08 09:58:52 +0300

    work static files on index.html

--- osbb_django/osbb_django/settings.py
+++ osbb_django/osbb_django/settings.py
@@ -68,6 +68,11 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "/static/"),
+    "C:/Users/sashok/PycharmProjects/OSBB/osbb_django/static"
+]
+
 WSGI_APPLICATION = 'osbb_django.wsgi.application'
 
 
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'osbb_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_osbb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
