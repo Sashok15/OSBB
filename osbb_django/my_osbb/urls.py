cc3f18e443aa7432098aec6e5b343faf37f6b820
Sashok15 <lordyhard@gmail.com>
2017-06-12 09:31:51 +0300

    added routes

--- osbb_django/my_osbb/urls.py
+++ osbb_django/my_osbb/urls.py
@@ -3,6 +3,8 @@ from my_osbb import views
 
 urlpatterns=[
     url(r'^index/$', views.index, name="index"),
-    url(r'^sign-in/$', views.sign_in, name="sign-in"),
-    url(r'^person-init/$', views.person_init, name="person-init")
+    url(r'^sign-in/person/$', views.login_person, name="loginPerson"),
+    url(r'^register/$', views.register_person, name="registerPerson"),
+    url(r'^sign-in/osbb/$', views.login_osbb, name='loginOsbb')
+
 ]
--- osbb_django/my_osbb/views.py
+++ osbb_django/my_osbb/views.py
@@ -3,14 +3,27 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-
 def index(request):
     return render(request, 'index.html')
 
 
-def sign_in(request):
-    return render (request, 'sign-in.html')
+def login_person(request):
+    return render (request, 'loginPerson.html')
 
 
-def person_init(request):
-    return render (request, 'person-init.html')
+def register_person(request):
+    return render (request, 'registerPerson.html')
+
+
+def login_osbb(request):
+    return render(request, 'loginOsbb.html')
+
+
+def osbb(request):
+    return render(request, 'osbb.html')
+
+
+def osbb_list(request):
+    return render(request, 'osbblist.html')
+
+
